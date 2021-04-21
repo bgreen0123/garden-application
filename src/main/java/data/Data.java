1aecb79ffe17c6f0fce5afc9ac2b60177f9e9dbe
Adam K <adam.kenney531@gmail.com>
2021-04-21 17:43:39 -0400

    new Alpha branch

--- src/main/java/data/Data.java
+++ src/main/java/data/Data.java
@@ -16,7 +16,7 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.w3c.dom.Element; 
+import org.w3c.dom.Element;      
 
 import javax.xml.parsers.DocumentBuilder;
 
