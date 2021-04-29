9941bd4636727d8b1d8b745adc02621e888ffa43
cplum <cplum@udel.edu>
2021-04-29 13:40:55 -0400

    Model tests verified

--- src/test/java/TestModel.java
+++ src/test/java/TestModel.java
@@ -13,79 +13,39 @@ public class TestModel {
 	//private ArrayList<Lep> lepsTest = new ArrayList<Lep>();
 	//private int budgetLeft = 30;
 	Plant plant1 = new Plant("Plant 1","Plant 1",34,PlantType.WOODY, Soil.CLAY, Sun.FULLSUN, Moisture.WET, "ImageURL");
-	Lep lep1 = new Lep(0,0,0,0);
 	Model m = new Model();
 	
-	
-	@Test
-	public void testUpdateVelocities() {
-		lep1.updateVelocities(2);
-		System.out.println("update x velocity");
-		assertEquals(2,lep1.getXVelocity());
-		System.out.println("update y velocity");
-		assertEquals(2,lep1.getYVelocity());
-	}
-	
-	@Test
-	public void testUpdateLocation() {
-		System.out.println("update x location");
-		assertEquals(25,27);
-		System.out.println("update y location");
-		assertEquals(25,27);
-	}
-	
-	@Test
-	public void testUpdateBudget() {
-		System.out.println("test case update budget");
-		m.updateBudget(29);
-		assertEquals(0, m.getBudget());
-	}
-	
-	@Test
-	public void testAddLeps() {
-		System.out.println("test case add leps");
-		m.addLeps(lep1);
-		
-		assertEquals(1, m.getNumLeps());
-	}
-	
-	@Test
-	public void testRemovePlant() {
-		System.out.println("test remove plant");
-		m.removePlant(plant1);
-		assertEquals(1, m.getPlants().size());
-	}
-	
-	@Test
+	@Test 
 	public void testAddPlant() {
-		System.out.println("test add plant");
-		
 		m.addPlant(plant1);
-		m.addPlant(plant1);
-		
-		assertEquals(0, m.getLeps().size());
-		fail();
+		assertEquals(m.getPlants().size(), 1);
 	}
 	
 	@Test
-	public void testRemoveLeps() {
-		System.out.println("test remove lep");
-		
-		m.removeLep(lep1);
-		
-		assertEquals(3, m.getPlants().size());
-		fail();
+	public void testAddFavoritePlant() {
+		m.addFavoritePlant(plant1);
+		assertEquals(m.getFavorites().size(), 1);
 	}
-
+	
+	//DELETE REMOVEPLANT
+	
 	@Test
-	public void testGardenGetSet() {
-		System.out.println("test garden getter and setter");
+	public void testGetThread() {
+		assertTrue(m.getThread() instanceof DataThread);
+	}
+	
+	@Test
+	public void testGetData() {
+		assertTrue(m.getData() instanceof Data);
+	}
+	
+	@Test
+	public void testLepCountGetSet() {
+		System.out.println("test lepCount getter and setter");
 		
-		Gridspace plot[][] = new Gridspace[2][2];
-		m.setGarden(plot);
+		m.updateLepCount(5);
 		
-		assertSame(plot, m.getGarden());
-		fail();
+		assertEquals(5, m.getNumLeps());
 	}
 	
 	@Test
@@ -97,8 +57,6 @@ public class TestModel {
 		
 		assertEquals(100, m.getHeight());
 		assertEquals(200, m.getWidth());
-
-		fail();
 	}
 	
 	@Test
@@ -108,8 +66,6 @@ public class TestModel {
 		m.setSun(Sun.FULLSUN);
 		
 		assertEquals(Sun.FULLSUN, m.getSun());
-
-		fail();
 	}
 	
 	@Test
@@ -119,8 +75,6 @@ public class TestModel {
 		m.setSoil(Soil.DIRT);
 		
 		assertEquals(Soil.DIRT, m.getSoil());
-
-		fail();
 	}
 	
 	@Test
@@ -130,19 +84,6 @@ public class TestModel {
 		m.setMoisture(Moisture.WET);
 		
 		assertEquals(Moisture.WET, m.getMoisture());
-
-		fail();
-	}
-	
-	@Test
-	public void testLepCountGetSet() {
-		System.out.println("test lepCount getter and setter");
-		
-		m.updateLepCount(5);
-		
-		assertEquals(5, m.getNumLeps());
-
-		fail();
 	}
 	
 	@Test
@@ -152,38 +93,5 @@ public class TestModel {
 		m.updateBudget(500);
 		
 		assertEquals(500, m.getBudget());
-
-		fail();
 	}
-	
-	@Test
-	public void testFavoriteGetSet() {
-		System.out.println("test fav plant getter and setter");
-		
-		m.addFavoritePlant(plant1);
-		
-		assertEquals(1, m.getFavorites().size());
-
-		fail();
-	}
-	
-	@Test
-	public void testDataGet() {
-		System.out.println("test data getter");
-				
-		assertTrue(m.getData() instanceof Data);
-
-		fail();
-	}
-	
-	@Test
-	public void destroy() {
-		assertEquals("destroying all plants",0,m.getPlants().size());
-		assertEquals("destroying all plants",0,m.getLeps().size());
-		fail();
-	}
-	
-
-
-
 }
